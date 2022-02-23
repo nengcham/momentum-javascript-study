7fa6087efdcd616e2728d1c0b8fdd0da400d6686
nengcham <nengcham7@gmail.com>
2022-02-23 11:59:18 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// a+b a-b a*b a/b
+const calculateor = {
+    add: function(num1, num2){
+        console.log(num1 + num2)
+    },
+    sub: function(num1, num2){
+        console.log(num1 - num2)
+    },
+    mul: function(num1, num2){
+        console.log(num1 * num2)
+    },
+    div: function(num1, num2){
+        console.log(num1 / num2)
+    },
+}
+
+
+calculateor.add(1, 2)
+calculateor.sub(1, 2)
+calculateor.mul(1, 2)
+calculateor.div(1, 2)
